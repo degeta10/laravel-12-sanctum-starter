c6c8dbb3dfaa43fd5bb9dd231706d6c79f2ceae3
Devender <degeta10@gmail.com>
2026-03-01 02:56:09 +0530

    Remove unused files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Response;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->success(
+        Response::HTTP_OK,
+        "API is running"
+    );
 });
